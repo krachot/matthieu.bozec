5bfe999a79561c6d0adccd8c679a7468345fedc4
Matthieu Bozec <matthieu@bozec.org>
2024-04-17 20:52:30 +0200

    Refactor data

---
+++ src/data/metadata.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "title": "Matthieu Bozec",
+    "url": (process.env.CONTEXT === "production" ? process.env.URL : process.env.DEPLOY_PRIME_URL) ||
+        "http://localhost:8080",
+    "language": "fr",
+    "description": "Matthieu Bozec développeur full stack Symfony et Wordpress à Poitiers",
+    "author": {
+      "name": "Matthieu Bozec",
+      "email": "matthieu@4h04.com",
+      "url": "https://matthieu.bozec.org/about"
+    }
+}
